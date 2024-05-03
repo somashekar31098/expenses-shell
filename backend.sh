3fab3b561c53d03523fc7dba49274b10a60f9478
somashekar31098 <somashekar31098@gmail.com>
2024-05-03 11:20:16 +0530

    mysql

---
+++ backend.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+USERID=(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+
+Validate(){
+    if [ $1 -ne 0 ]
+  then 
+    echo -e "$2 is ....$R failure $N"
+    exit 1
+ else
+    echo -e "$2 is ....$G sucess $N"
+    fi
+}
+
+if [ $USERID -ne  0 ]
+then 
+   echo "run the script with root access"
+   exit 1
+else
+   echo "you are super user"
+fi
+
+dnf module disable nodejs -y
+Validate $? "disabling nodejs"
+
+dnf module enable nodejs:20 -y
+Validate $? "enabling nodejs"
+
+dnf install nodejs -y
+Validate $? "installing nodejs"
+
+useradd expense
+Validate $? "ceating expense user"
+
