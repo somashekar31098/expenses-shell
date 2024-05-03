61b5d6367b39a85435d9ef31b5e37f9eb09abbdb
somashekar31098 <somashekar31098@gmail.com>
2024-05-03 11:07:55 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -41,9 +41,8 @@ Validate $? "STARTING MYSQL"
 # Validate $? "SETTING ROOT PASSWORD OF MYSQL"
 
 #idempotancy nature code
- mysql -h db.daws1998.online -u root ${mysql_root_password}
- -e 'SHOW DATABASES;'
- if [ $? -ne 0]
+ mysql -h db.daws1998.online -u root ${mysql_root_password} -e 'SHOW DATABASES;'
+ if [ $? -ne 0 ]
    then 
       mysql_secure_installation --set-root-pass ${mysql_root_password} &>>LOGFILE
       Validate $? "root password setup"
